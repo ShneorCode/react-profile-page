8ae2156a3e01ad0ee7344c168088f4dd93acc261
Shneor <mm4471661@gmail.com>
2025-08-26 17:36:08 +0300

    commit 5

--- src/comps/ProfileCard.tsx
+++ src/comps/ProfileCard.tsx
@@ -4,7 +4,7 @@ export default function ProfileCard() {
   return (
     <>
       <div className="ProfileCard">
-        <img className="img" src="img01.jpg"></img>
+        <img className="img" src="img01.jpg" width={50} height={50}></img>
         <p className="name">name</p>
         <p className="description">description</p>
         <Button />
