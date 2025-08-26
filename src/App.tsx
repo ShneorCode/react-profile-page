b04054278cc945ddb25324bd72d7362d6cb79ffa
Shneor <mm4471661@gmail.com>
2025-08-26 15:51:01 +0300

    commit 1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./comps/Header";
+import ProfileCard from "./comps/ProfileCard";
+import Button from "./comps/Button";
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+      <ProfileCard></ProfileCard>
+    </div>
+  );
+}
---
+++ src/comps/Header.tsx
@@ -0,0 +1,5 @@
+import "../App.css";
+
+export default function Header() {
+  return <div className="header">My Profile Page</div>;
+}
